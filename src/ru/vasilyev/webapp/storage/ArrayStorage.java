6212dce434e7054c8197cc1360a6ceaba643025f
Lex <alexreferal@gmail.com>
2018-02-15 20:12:36 +0100

    static metchod

--- src/ru/vasilyev/webapp/storage/ArrayStorage.java
+++ src/ru/vasilyev/webapp/storage/ArrayStorage.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -28,7 +29,7 @@ public class ArrayStorage {
 
     public void save(Resume r) {
 
-        if (size == storage.length) {
+        if (size == STORAGE_LIMIT) {
             System.out.println("storage full!, not save!");
         } else {
             if (searchIndex(r.getUuid()) == -1) {
