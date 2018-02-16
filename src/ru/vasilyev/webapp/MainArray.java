cce3f8d24a1e175e0f7cd0c8a2c9a8b0b91342f9
Lex <alexreferal@gmail.com>
2018-02-16 22:59:08 +0100

    les 4 string exeption

--- src/ru/vasilyev/webapp/MainArray.java
+++ src/ru/vasilyev/webapp/MainArray.java
@@ -8,9 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Test for com.urise.webapp.storage.ru.vasilyev.webapp.storage.ArrayStorage
- */
+
 public class MainArray {
     private final static Storage ARRAY_STORAGE = new ArrayStorage();
 
@@ -18,7 +16,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | update uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -36,14 +34,12 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume();
-                    r.setUuid(uuid);
+                    r = new Resume(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
                 case "update":
-                    r = new Resume();
-                    r.setUuid(uuid);
+                    r = new Resume(uuid);
                     ARRAY_STORAGE.update(r);
                     printAll();
                     break;
@@ -67,7 +63,7 @@ public class MainArray {
         }
     }
 
-    private static void printAll() {
+    static void printAll() {
         Resume[] all = ARRAY_STORAGE.getAll();
         System.out.println("----------------------------");
         if (all.length == 0) {
---
+++ src/ru/vasilyev/webapp/exception/ExistStorageException.java
@@ -0,0 +1,7 @@
+package ru.vasilyev.webapp.exception;
+
+public class ExistStorageException extends StorageException {
+    public ExistStorageException(String uuid) {
+        super("Resume " + uuid + " already exist", uuid);
+    }
+}
