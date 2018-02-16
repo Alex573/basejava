2e336faa54dc171af806ed4ca47e106b2d889639
Lex <alexreferal@gmail.com>
2018-02-16 22:00:37 +0100

    HW3 ready

--- src/ru/vasilyev/webapp/storage/ArrayStorage.java
+++ src/ru/vasilyev/webapp/storage/ArrayStorage.java
@@ -9,72 +9,20 @@ import java.util.Arrays;
  */
 public class ArrayStorage extends AbstractArrayStorage implements Storage {
 
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    public void update(Resume r) {
-        int res = searchIndex(r.getUuid());
-        if (res != -1) {
-            storage[res] = r;
-        } else {
-            System.out.println("Resume " + r.getUuid() + " not found");
-        }
-
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
     }
 
-    public void save(Resume r) {
-
-        if (size == STORAGE_LIMIT) {
-            System.out.println("storage full!, not save!");
-        } else {
-            if (searchIndex(r.getUuid()) == -1) {
-                storage[size] = r;
-                size++;
-            } else {
-                System.out.println("DataBase have: " + r.getUuid() + " not save");
-            }
-        }
-
-    }
-
-    public Resume get(String uuid) {
-        int res = searchIndex(uuid);
-        if (res != -1) {
-            return storage[res];
-        } else {
-            System.out.println("Resume " + uuid + " not found");
-        }
-        return null;
-
-    }
-
-
-    public void delete(String uuid) {
-        int res = searchIndex(uuid);
-        if (res != -1) {
-            storage[res] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        } else {
-            System.out.println("Resume " + uuid + " not found");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
-
-
-    protected int searchIndex(String uuid) {
+    @Override
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
