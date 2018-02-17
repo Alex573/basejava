9d8c8a6e2808173b8a98de354e109373a05ff46c
Lex <alexreferal@gmail.com>
2018-02-17 18:40:29 +0100

    Lesson4 collection

--- test/ru/vasilyev/webapp/storage/AbstractArrayStorageTest.java
+++ test/ru/vasilyev/webapp/storage/AbstractArrayStorageTest.java
@@ -8,22 +8,29 @@ import ru.vasilyev.webapp.exception.NotExistStorageException;
 import ru.vasilyev.webapp.exception.StorageException;
 import ru.vasilyev.webapp.model.Resume;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractArrayStorageTest {
     private Storage storage;
 
     private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-
     private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-
     private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
-
     private static final String UUID_4 = "uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID_4);
+
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+    private static final Resume RESUME_4;
+
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        RESUME_4 = new Resume(UUID_4);
+    }
+
 
     protected AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -31,6 +38,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Before
     public void setUp() throws Exception {
+
         storage.clear();
         storage.save(RESUME_1);
         storage.save(RESUME_2);
