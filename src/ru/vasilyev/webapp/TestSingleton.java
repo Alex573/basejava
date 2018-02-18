cc6b6a3ef0d6194e6f69acb4ace3cedb1419fb2b
Lex <alexreferal@gmail.com>
2018-02-18 03:10:46 +0100

    Lesson7 enum

---
+++ src/ru/vasilyev/webapp/TestSingleton.java
@@ -0,0 +1,31 @@
+package ru.vasilyev.webapp;
+
+import ru.vasilyev.webapp.model.SectionType;
+
+public class TestSingleton {
+    private static TestSingleton instance;
+
+    private static TestSingleton getInstance() {
+        if (instance == null) {
+            instance = new TestSingleton();
+        }
+        return instance;
+    }
+
+    private TestSingleton() {
+    }
+
+    public static void main(String[] args) {
+        String s = TestSingleton.getInstance().toString();
+        Singleton instance = Singleton.valueOf("INSTANCE");
+        System.out.println(instance.ordinal());
+
+        for (SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
+    }
+
+    public enum Singleton {
+        INSTANCE
+    }
+}
